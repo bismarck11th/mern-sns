c3affd42c523f62dba5de512c0cb05db928a0bc9
kazuki827 <xm.kazuki827@gmail.com>
2022-05-01 21:57:35 +0900

    logging (log4js)

--- server/index.js
+++ server/index.js
@@ -3,13 +3,19 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import log4js from 'log4js';
 
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/user.js';
 
-const app = express();
 dotenv.config();
 
+log4js.configure('./log4js_setting.json', { reloadSecs: 300 });
+const logger = log4js.getLogger();
+
+const app = express();
+
+app.use(log4js.connectLogger(logger, { level: log4js.levels.DEBUG }));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
@@ -23,6 +29,7 @@ mongoose
   .connect(process.env.CONNECTION_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    logger.debug('サーバー起動', `http://localhost:${PORT}`);
   })
   .catch((error) => {
     console.log(error.message);
